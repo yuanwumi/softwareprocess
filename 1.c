e11cc466e0034848c500a4df84269caa8bf5da34
元武丿秘 <73014546+yuanwumi@users.noreply.github.com>
2023-04-28 22:47:37 +0800

    最大连续子数组和

---
+++ 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int n,x[100000],max,sum,i;
+	scanf("%d",&n);
+	max = 0;
+	sum = 0;
+	for (i = 0; i < n; i++) {
+		scanf("%d", &x[i]);
+	}
+	for (i = 0; i < n; i++) {
+		sum = sum + x[i];
+		if (sum > max) {
+			max = sum;
+		}
+		if (sum < 0) {
+			sum = 0;
+		}
+	}
+	printf("%d",max);
+	return 0;
+}
